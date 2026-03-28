71859506d3f8b15eddb72d86cc855cb3036b4255
Natasha <zhorova.tasha@gmail.com>
2026-03-28 19:24:04 +0300

    Исправлены ссылки на Minio, обновлены настройки и стили

--- bmstu/settings.py
+++ bmstu/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "bmstu.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'student',
+        'USER': 'student',
+        'PASSWORD': 'student',
+        'HOST': 'localhost',  # или '127.0.0.1'
+        'PORT': '5432',
     }
 }
 
