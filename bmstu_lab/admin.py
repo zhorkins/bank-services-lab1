7011e7a56e66c36b399601b09bae18bdf8be1a03
Natasha <zhorova.tasha@gmail.com>
2026-03-28 18:40:25 +0300

    Удалены старые папки bmstu и bmstu_lab

--- bmstu_lab/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from bmstu_lab.models import Product, Order, ProductInOrder
-
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(ProductInOrder)
-
-# Register your models here.
